0fa299a02cee368d5acae9b4042824a738732cc5
Abhishek kumar <44266567+abhishek7870@users.noreply.github.com>
2022-12-12 18:25:49 +0530

    tag and some changes in recipe

--- recipe/urls.py
+++ recipe/urls.py
@@ -9,7 +9,8 @@ from recipe import views
 
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
-router.register('recipe',views.RecipeViewSet)
+router.register('recipes',views.RecipeViewSet)
+router.register('tags',views.TagViewSet)
 
 app_name = 'recipe'
 
--- recipe/views.py
+++ recipe/views.py
@@ -2,12 +2,19 @@
 view for recipe api
 """
 
-from rest_framework import viewsets
+from rest_framework import(
+    viewsets,
+    mixins,
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import(
+    Recipe,
+    Tag,
+)
 from recipe import serializers
+
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """view for manage recipe api"""
@@ -29,3 +36,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """creating a new recipe"""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.DestroyModelMixin, mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+    """managing tag in database"""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """filter queryset for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
